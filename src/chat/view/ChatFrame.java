88c9948886c2e398e32688b337d82b9bc0f97ce7
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-11-13 09:44:07 -0700

    Added code for JFrame.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -4,6 +4,19 @@ import chat.controller.ChatbotController;
 import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
-{
-
+{	
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setVisible(true);
+	}
 }
